dd6e3bbd9e7dde2ef4256c7ebaff3290d7222df3
tcarr-icloud <carr.timothy@gmail.com>
2025-11-14 14:09:32 -0700

    Updated env config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,8 +3,8 @@ export const environment = {
   apiUrl: 'http://localhost:8081/api',
   oidc: {
     realm: 'development',
-    authority: 'http://keycloak:8080/realms/development',
+    authority: 'http://localhost:8080/realms/development',
     clientId: 'spa-client',
-    userinfo_endpoint: 'http://keycloak:8080/realms/development/protocol/openid-connect/userinfo',
+    userinfo_endpoint: 'http://localhost:8080/realms/development/protocol/openid-connect/userinfo',
   }
 };
