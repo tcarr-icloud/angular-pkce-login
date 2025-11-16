901e2c7dc0ece75de2f7774ee7ea304baac67c4f
tcarr-icloud <carr.timothy@gmail.com>
2025-11-16 03:52:13 -0700

    oidc default to port 9000

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,8 +3,8 @@ export const environment = {
   apiUrl: 'http://localhost:8081/api',
   oidc: {
     realm: 'development',
-    authority: 'http://localhost:8080/realms/development',
+    authority: 'http://localhost:9000/realms/development',
     clientId: 'spa-client',
-    userinfo_endpoint: 'http://localhost:8080/realms/development/protocol/openid-connect/userinfo',
+    userinfo_endpoint: 'http://localhost:9000/realms/development/protocol/openid-connect/userinfo',
   }
 };
